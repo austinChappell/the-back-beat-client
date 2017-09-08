aa8f7d9b796872168c172fdd141cb33347ebb32c
austinChappell <Austin.W.Chappell@gmail.com>
2017-09-08 09:02:51 -0500

    add features section

---
+++ src/components/Features.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Features extends Component {
+  render() {
+    return (
+      <div className="Features">
+        <h1>Features</h1>
+        <div className="feature-list">
+          <div className="feature">
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <h2>Find local musicians</h2>
+            <p>Search by genre, instrument, or skill level.</p>
+          </div>
+          <div className="feature">
+            <i className="fa fa-music" aria-hidden="true"></i>
+            <h2>Get connected</h2>
+            <p>Our "Performed With" feature lets you connect with those you've shared the stage with.</p>
+          </div>
+          <div className="feature">
+            <i className="fa fa-calendar" aria-hidden="true"></i>
+            <h2>Share your gigs</h2>
+            <p>Keep track of your own gigs or follow other musicians' calendars.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Features;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
+import Features from './Features';
 import SignUpForm from './SignUpForm';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <div className="overlay">
+        <div className="landing-page-background">
           <div className="feature-text">
             <h1>The Back Beat</h1>
             <h2>Connecting musicians<br />in a digital age.</h2>
           </div>
           <SignUpForm />
         </div>
+        <Features />
       </div>
     )
   }
