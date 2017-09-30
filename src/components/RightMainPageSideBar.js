b76cb11db1ddace9edf5d03c44f60a9ed7067476
austinChappell <Austin.W.Chappell@gmail.com>
2017-09-30 14:32:53 -0500

    wip browsebox

--- src/components/RightMainPageSideBar.js
+++ src/components/RightMainPageSideBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import BrowseBox from './BrowseBox';
 import UserBox from './UserBox';
@@ -33,17 +34,20 @@ class RightMainPageSideBar extends Component {
     }).then((response) => {
       return response.json();
     }).then((data) => {
-      console.log('DATA', data);
-      this.setState({
-        musicians: data
-      });
+      const currentMusician = data[0];
+      currentMusician.name = `${currentMusician.first_name} ${currentMusician.last_name}`;
+      currentMusician.url = `/profile/${currentMusician.username}`;
+      this.setState({ musicians: data, musician: currentMusician });
     });
   }
 
   swipe = (itemsArray, itemIndexName, direction) => {
+    console.log('SWIPE FUNCTION', direction);
     const updateObj = {};
-    if (this.state[itemIndexName] === this.state[itemsArray].length - 1) {
+    if (this.state[itemIndexName] === this.state[itemsArray].length - 1 && direction === 1) {
       updateObj[itemIndexName] = 0;
+    } else if (this.state[itemIndexName] === 0 && direction === -1) {
+      updateObj[itemIndexName] = this.state[itemsArray].length - 1;
     } else {
       updateObj[itemIndexName] = this.state[itemIndexName] + direction;
     }
@@ -52,19 +56,30 @@ class RightMainPageSideBar extends Component {
 
   render() {
 
+    let musician = {};
+    if (this.state.musicians.length > 0) {
+      musician = this.state.musicians[this.state.musicianIndex];
+    }
+
+    console.log('MUSICIAN', musician);
+
     return (
       <div className="RightMainPageSideBar">
 
         <UserBox />
 
         <BrowseBox
-          displayTitle={this.state.musician.name}
-          displayTitleURL={this.state.musician.url}
           goToPrev={() => this.swipe('musicians', 'musicianIndex', -1)}
-          goToPrev={() => this.swipe('musicians', 'musicianIndex', 1)}
+          goToNext={() => this.swipe('musicians', 'musicianIndex', 1)}
           index={this.state.eventIndex}
           title="Musicians Near You"
-        />
+        >
+          <h3>
+            <Link onClick={() => this.props.updateUser(musician)} to={`/profile/${musician.username}`}>{musician.first_name} {musician.last_name}</Link>
+          </h3>
+          <span><strong>City:</strong> {musician.city}</span> <br />
+          <span><strong>Skill:</strong> {musician.skill_level}</span>
+        </BrowseBox>
 
         <BrowseBox
           goToPrev={this.goToPrev}
@@ -86,7 +101,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    updateUser: (user) => {
+      const action = { type: 'UPDATE_USER', user };
+      dispatch(action);
+    }
   }
 }
 
