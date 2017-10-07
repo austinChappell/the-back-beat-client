1f855f20f7470326b443bb21bf1405ba4b119ea5
austinChappell <Austin.W.Chappell@gmail.com>
2017-10-07 16:51:07 -0500

    scrollable sidebar

---
+++ src/components/Main2.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import LeftMainPageSideBar from './LeftMainPageSideBar';
+import NewsFeed from './NewsFeed';
+import RightMainPageSideBar from './RightMainPageSideBar';
+
+class Main extends Component {
+
+  componentDidMount() {
+    const apiURL = this.props.apiURL;
+    fetch(`${apiURL}/api/profile/${this.props.currentUsername}`, {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((results) => {
+      this.props.updateUser(results);
+      window.localStorage.username = results.username;
+      this.props.setOnboardingStage(results.onboarding_stage);
+    })
+  }
+
+  componentDidUpdate() {
+    if (this.props.onboardingStage <= this.props.onboardingMaxStage) {
+      this.props.history.push('/onboarding');
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="Main">
+        <LeftMainPageSideBar />
+        <NewsFeed />
+        <RightMainPageSideBar />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    apiURL: state.apiURL,
+    currentUsername: state.currentUsername,
+    currentUser: state.currentUser,
+    onboardingStage: state.onboardingStage,
+    onboardingMaxStage: state.onboardingMaxStage
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setOnboardingStage: (stage) => {
+      const action = { type: 'SET_ONBOARDING_STAGE', stage };
+      dispatch(action);
+    },
+
+    updateUser: (results) => {
+      const action = { type: 'UPDATE_USER', user: results };
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
