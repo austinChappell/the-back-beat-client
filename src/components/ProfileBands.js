d580347d3e5766e4745d1a5836413305a10985b5
austinChappell <Austin.W.Chappell@gmail.com>
2017-10-07 23:58:00 -0500

    profile bands styling

--- src/components/ProfileBands.js
+++ src/components/ProfileBands.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class ProfileBands extends Component {
 
   // TODO: START BACK HERE. CURRENT USER EVENTS WILL RPOBABLY NEED TO BE STORED IN REDUX. THIS IS ALSO RELATED TO THE BUG WHEN NOT CHANGING BACK TO LOGGED IN USER INFO
 
+  state = {
+    bands: []
+  }
+
+  componentDidMount() {
+    this.fetchUserBands();
+  }
+
+  fetchUserBands = () => {
+    const url = this.props.apiURL;
+    fetch(`${url}/api/bands/user/${this.props.currentUser.id}`, {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((results) => {
+      console.log(results.rows);
+      this.setState({ bands: results.rows });
+    })
+  }
+
   render() {
     return (
       <div className="ProfileBands">
-        Profile Bands Component
+        <div className="band-results">
+          {this.state.bands.map((band, index) => {
+            return (
+              <div key={index} className="band">
+                <h3><Link to={`/band/${band.band_id}`}>{band.band_name}</Link> - {band.band_genre}</h3>
+                <h4>{band.band_city}</h4>
+                <p>{band.band_description}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
@@ -16,7 +50,8 @@ class ProfileBands extends Component {
 
 const mapStateToProps = (state) => {
   return {
-
+    apiURL: state.apiURL,
+    currentUser: state.currentUser
   }
 }
 
--- src/components/ProfileInfoMain.js
+++ src/components/ProfileInfoMain.js
@@ -10,7 +10,6 @@ class ProfileInfoMain extends Component {
       <div className="ProfileInfoMain">
         <div className="profile-info">
           <h3>{user.first_name} {user.last_name}</h3>
-          <h4>Email: {user.email}</h4>
           <h4>City: {user.city}</h4>
           <h4>Skill Level: {user.skill_level}</h4>
           <h4>Instruments: </h4>
