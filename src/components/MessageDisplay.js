bcb188443911487239de5242bf587747ecb9ed3d
austinChappell <Austin.W.Chappell@gmail.com>
2017-12-19 08:02:31 -0600

    wip

--- src/components/MessageDisplay.js
+++ src/components/MessageDisplay.js
@@ -24,13 +24,13 @@ class MessageDisplay extends Component {
   }
 
   componentDidUpdate() {
-    // this.refs.messageBox.scrollTop = 200;
+    // this.refs.chatWindow.scrollTop = 200;
     let selectedMessages = this.props.selectedMessages ? this.props.selectedMessages : [];
     if (this.props.currentRecipient && selectedMessages.length !== this.state.numOfSelectedMessages) {
       this.setState({
         numOfSelectedMessages: this.props.selectedMessages.length
       }, () => {
-        this.refs.messageBox.scrollTop = this.refs.messageBox.scrollHeight;
+        this.refs.chatWindow.scrollTop = this.refs.chatWindow.scrollHeight;
       })
     }
     this.getUnreadMessages();
@@ -88,7 +88,8 @@ class MessageDisplay extends Component {
 
   render() {
 
-    console.log('MESSAGE DISPLAY PROPS', this.props);
+    const now = new Date();
+    const today = now.toDateString();
 
     const loggedInUserId = this.props.loggedInUser.id;
     // console.log('MY ID', loggedInUserId);
@@ -96,25 +97,51 @@ class MessageDisplay extends Component {
     let messageDisplay;
     let selectedMessages = this.props.selectedMessages ? this.props.selectedMessages : [];
 
-    messageDisplay = <div className="message-box">
-      <div className={this.props.currentRecipient ? 'currentRecipient' : ''}>
-        {recipient}
-      </div>
-      <div className="messages" ref="messageBox">
-        {selectedMessages.map((message, index) => {
-          return (
-            <div key={index} className={message.sender_id === loggedInUserId ? "message sent" : "message received"}>
-              <div className={message.sender_id === loggedInUserId ? "message-wrapper sent" : "message-wrapper received"}>
+    messageDisplay = <div className="BandChat">
+      <section className="chat-section">
+        <div className={this.props.currentRecipient ? 'currentRecipient' : ''}>
+          {recipient}
+        </div>
+        <div className="chat-window" ref="chatWindow">
+          {selectedMessages.map((message, index) => {
+            const date = new Date(message.created_at);
+            if (date.toDateString() === today) {
+              message.date = 'Today';
+            } else {
+              message.date = date.toDateString();
+            }
+            message.time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            let dateClass = 'date';
+            for (let i = 0; i < index; i++) {
+              if (selectedMessages[i].date === message.date) {
+                dateClass = 'hide';
+              }
+            }
+            return (
+              <div key={index} className="message">
+                <div className={dateClass}>
+                  <hr />
+                  <span>{message.date}</span>
+                  <hr />
+                </div>
+                <div className="time">
+                  <div className="user">
+                    <img src={message.profile_image_url} /><span>{message.first_name} {message.last_name}</span>
+                  </div>
+                  <div className="time-stamp">
+                    <span>{message.time}</span>
+                  </div>
+                </div>
                 <p>{message.message_text}</p>
               </div>
-            </div>
-          );
-        })}
-      </div>
-      <MessageInput
-        parentName="MessageDisplay"
-        sendMessage={this.sendMessage}
-      />
+            );
+          })}
+        </div>
+        <MessageInput
+          parentName="MessageDisplay"
+          sendMessage={this.sendMessage}
+        />
+      </section>
     </div>
 
     return (
